c17b41b5c7e66634b16cc27da9395b2a118b1cce
dExter205 <88722109+dExter205@users.noreply.github.com>
2021-08-13 09:05:19 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
-let MINUTOS = 0
 let SEGUNDOS = 0
+let MINUTOS = 0
+let HORA = 0
 basic.forever(function () {
     basic.pause(100)
     SEGUNDOS += 1
     if (SEGUNDOS == 59) {
         SEGUNDOS = 0
-        MINUTOS += 1
+        MINUTOS += 59
+    }
+    if (MINUTOS == 59) {
+        MINUTOS = 0
+        HORA += 1
+    }
+    if (HORA == 12) {
+    	
     }
 })
